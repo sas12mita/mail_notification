82b952bdbb8ce57da01515fa8f6f32a09cc82c56
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-10 22:02:45 +0545

    Make model and migration

---
+++ app/Http/Controllers/BrokerAccountController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BrokerAccount;
+use Illuminate\Http\Request;
+
+class BrokerAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BrokerAccount $brokerAccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BrokerAccount $brokerAccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BrokerAccount $brokerAccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BrokerAccount $brokerAccount)
+    {
+        //
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PhpParser\Node\Expr\FuncCall;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function bankAccounts()
+    {
+        return $this->hasMany(BankAccount::class);
+    }
+    public function brokerAccounts()
+    {
+        return $this->hasMany(BrokerAccount::class);
     }
 }
